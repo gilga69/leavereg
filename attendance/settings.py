e7b22e820b0daee1dde35325250a32a6cd3a5578
Ganesh Sharma <sosukegani.tmk@gmail.com>
2017-09-16 15:47:29 +0530

    HKS second commit

--- attendance/settings.py
+++ attendance/settings.py
@@ -125,7 +125,23 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'H:\Internship\editing\media'
 MEDIA_URL = '/media/'
+
+
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
