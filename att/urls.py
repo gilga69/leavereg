6c88f14a767fe115f64e5cbdf38e010c423383b8
Ganesh Sharma <sosukegani.tmk@gmail.com>
2017-09-16 16:37:07 +0530

    HKS Leave Register

--- att/urls.py
+++ att/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='att'
 
 urlpatterns=[
-    url(r'^',views.index,name='index'),
+    url(r'^$',views.index,name='index'),
     url(r"^register/",views.attendancereg,name='att'),
     url(r'^viewall/',views.ulogin,name='all_attendance'),
     url(r'^allleavelist/',views.attview,name='attview'),
