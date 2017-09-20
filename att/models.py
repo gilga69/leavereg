ba9ca032ac3e1532f3b7a6bf9616af59ac4d1965
Ganesh Sharma <sosukegani.tmk@gmail.com>
2017-09-20 10:01:24 +0530

    Blank

--- att/models.py
+++ att/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 
 class attsheet(models.Model):
     name=models.CharField(max_length=30)
     contact_no=models.CharField(max_length=10)
-    room_no=models.IntegerField()
+    room_no=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(25)])
     from_date=models.DateField()
     to_date=models.DateField()
     purpose=models.TextField(max_length=500)
